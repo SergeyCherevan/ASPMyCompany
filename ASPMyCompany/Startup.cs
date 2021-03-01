b65f240109125df4f6843a905abdcc5a5d892410
SergeyCherevan <CherevanSergeyLugansk@gmail.com>
2021-03-02 00:10:04 +0200

    Создание контроллера и представления. Установление EndPoint'а на контроллер.

--- ASPMyCompany/Startup.cs
+++ ASPMyCompany/Startup.cs
@@ -32,10 +32,7 @@ namespace ASPMyCompany
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
