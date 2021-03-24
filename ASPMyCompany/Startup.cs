5a0ee97e8597b86b39746c1b420760a4c3241565
SergeyCherevan <CherevanSergeyLugansk@gmail.com>
2021-03-24 20:01:23 +0200

    Добавили DataManager.cs
    Добавили сервисы моделей и поддержку БД в Startup.cs

--- ASPMyCompany/Startup.cs
+++ ASPMyCompany/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -9,7 +10,13 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
 using ASPMyCompany.Service;
+using ASPMyCompany.Domain.Repositories.Abstract;
+using ASPMyCompany.Domain.Repositories.EntityFramework;
+using ASPMyCompany.Domain;
 
 namespace ASPMyCompany
 {
@@ -24,8 +31,41 @@ namespace ASPMyCompany
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // подключаем конфигурацию проекта из appsettings.json к классу Config
             Configuration.Bind("Project", new Config());
 
+            // подключаем нужный функционал приложения в качестве сервисов
+            /* Зависимости транзитивные - в рамках одного http-запроса по требованию
+             * может быть создано сколько угодно объектов данного типа */
+            services.AddTransient<ITextFieldsRepository, EFTextFieldsRepository>();
+            services.AddTransient<IServiceItemsRepository, EFServiceItemsRepository>();
+            services.AddTransient<DataManager>();
+
+            // подключаем БД через нашу строчку из appsettings.json
+            services.AddDbContext<AppDbContext>(x => x.UseSqlServer(Config.ConnectionString));
+
+            // настраиваем identity-систему идентификацию
+            services.AddIdentity<IdentityUser, IdentityRole>(option =>
+            {
+                option.User.RequireUniqueEmail = true;
+                option.Password.RequiredLength = 6;
+                option.Password.RequireNonAlphanumeric = false;
+                option.Password.RequireLowercase = false;
+                option.Password.RequireUppercase = false;
+                option.Password.RequireDigit = false;
+            }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+            // настраиваем authentification cookie
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.Name = "MyCompanyAuth";
+                options.Cookie.HttpOnly = true;
+                options.LoginPath = "/account/login";
+                options.AccessDeniedPath = "/account/accessdenied";
+                options.SlidingExpiration = true;
+            });
+
+            // добавляем поддержку контроллеров
             services.AddControllersWithViews()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
@@ -33,12 +73,21 @@ namespace ASPMyCompany
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //если проект в стадии разработки, подключаем страницу отабражения ошибок
             if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
 
+            // подключаем систему маршрутизации (может нужно поменять местами со следующим)
             app.UseRouting();
 
+            // подключаем поддержку статических файлов
             app.UseStaticFiles();
 
+            // подключаем аутентификацию и авторизацию
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            // подключаем необходимые машруты - эндпоинты
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
